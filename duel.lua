2f78f132c005db16c64bff1c21e3ad071cbfc8f3
dansimco <ds@dansim.co>
2021-01-26 13:59:59 +0700

    softcut working

--- duel.lua
+++ duel.lua
@@ -43,13 +43,16 @@ local buffers = {"A", "B"}
 local time_length_options = {1, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
 local time_scale_options = {"1/8", "1", "+16"}
 
+params:add_option("reset_on_time_change","reset on time change",{"no", "yes"}, 2)
+
 for i=1,2 do
   params:add_separator("Buffer " .. buffers[i])
-  params:add_option(i.."time","buffer "..buffers[i].." time (beats)",time_length_options,9)
+  params:add_option(i.."time","buffer "..buffers[i].." time (beats)",time_length_options,7)
   params:add_option(i.."time_scale","buffer "..buffers[i].." time mod",time_scale_options,1)
-  params:add_number(i.."feedback", "buffer "..buffers[i].." feedback", 0, 110, 0)
-  params:add_number(i.."send_amount", "buffer "..buffers[i].." feedback", 0, 100, 0)
+  params:add_number(i.."feedback", "buffer "..buffers[i].." feedback", 0, 100, 75)
+  params:add_number(i.."send_amount", "buffer "..buffers[i].." send amount", 0, 100, 0)
 end
+
 
 function init()
   setup_softcut()
@@ -61,7 +64,11 @@ function init()
   screen_timer.time = 1/12
   screen_timer.event = function() redraw() end
   screen_timer:start()
-  
+
+  apply_pattern_division(1)
+  apply_pattern_division(2)
+
+
 end
 
 
@@ -105,12 +112,14 @@ end
 
 function apply_pattern_division(p)
   local divisor = time_length_options[params:get(p .. "time")]
-  patterns[p].division = 1/(baseline_division/divisor)
+
+  divisor = 360/(divisor * 8)
+  patterns[p].division = 1/divisor
 end
 
 
-function reset_loop(buffer)
-  print("reset loop " .. buffer)
+function reset_loop(voice)
+  softcut.position(voice, 0.5)
 end
 
 
@@ -122,62 +131,92 @@ end
 ----------------------------
 
 function setup_softcut()
+  rate = 1.0
+  rec = 1.0
+  pre = 0.5
 
-  audio.level_cut(1)
+  -- send audio input to softcut input
   audio.level_adc_cut(1)
+  audio.level_eng_cut(0)
   audio.level_tape_cut(0)
-  audio.level_eng_cut(1)
-
-  local buffer_pan = {-1, 1}
-
-  for si = 1,2 do
-
-    softcut.level(si,1)
-    softcut.level_slew_time(si,0.01)
-    softcut.level_input_cut(si, 1, 1.0)
-    softcut.level_cut_cut(si, si, 1.0)
-    softcut.rate(si, 1)
-    softcut.rate_slew_time(si,0.1)
-
-    softcut.loop_start(si, 0)
-    softcut.loop_end(si, 5)
-    softcut.loop(si, 1)
-    
-    softcut.fade_time(si, 0.01)
-    softcut.rec(si, 1)
-
-    softcut.rec_level(si, 1)
-    softcut.pre_level(si, 0.5)
-    
-    softcut.position(si, 0)
-    softcut.buffer(si,si)
-    
-    -- softcut.filter_dry(si, 1)
-    softcut.pan(si, buffer_pan[si])
-  end
+  
+  softcut.enable(1,1)
+  softcut.buffer(1,1)
+  softcut.level(1,1.0)
+  softcut.loop(1,1)
+  softcut.loop_start(1,0)
+  softcut.loop_end(1,120)
+  softcut.position(1,0)
+  softcut.play(1,1)
+  softcut.pan(1, -1)
+  softcut.rec(1,1)
+  softcut.pre_level(1,pre)
+  softcut.level_input_cut(1,1,1.0)
+  softcut.rec_level(1,rec)
 
 
-    -- softcut.buffer_clear()
-  softcut.enable(1, 1)
-  softcut.enable(2, 1)
-  softcut.play(1, 0)
-  softcut.play(2, 0)
+  softcut.enable(2,1)
+  softcut.buffer(2,2)
+  softcut.level(2,1.0)
+  softcut.loop(2,1)
+  softcut.loop_start(2,0)
+  softcut.loop_end(2,120)
+  softcut.position(2,0)
+  softcut.play(2,1)
+  softcut.pan(2, 1)
+  softcut.rec(2,1)
+  softcut.pre_level(2,pre)
+  softcut.level_input_cut(2,2,1.0)
+  softcut.rec_level(2,rec)
 
-  print("SOFTCUT GO")
+
+
+  softcut.voice_sync(1,2,0)
+
+
+
+  softcut.buffer_clear()
+
+
+
 end
+
+
+-- function update_positions(voice,position)
+--   print(voice,position)
+-- end
+
+-- softcut.phase_quant(1,0.1)
+-- -- softcut.phase_quant(2,0.1)
+-- softcut.event_phase(update_positions)
+-- softcut.poll_start_phase()
+
+
 
 ----------------------------
 --
 --  Norns Inputs
 --
 ----------------------------
+directions = {1, 1}
 
-function enc()
-
+function enc(n, d)
+  if n == 2 or n == 3 then
+    softcut.level_input_cut(n-1,n-1,util.clamp())
+  end
 end
 
 function key(n, z)
-  print(clock.get_beat_sec())
+  if n == 2 and z == 1 then
+    directions[1] = directions[1] * -1
+    softcut.rate(1,directions[1])
+  end
+
+  if n == 3 and z == 1 then
+    directions[2] = directions[2] * -1
+    softcut.rate(2,directions[2])
+  end
+
 end
 
 
@@ -200,21 +239,29 @@ end
 function a.delta(n, d)
   if (n == 2) then
     params:delta("1feedback", d/11)
+    softcut.pre_level(1,params:get("1feedback")/100)
   end
 
   if (n == 3) then
     params:delta("2feedback", d/11)
+    softcut.pre_level(2,params:get("2feedback")/100)
   end
 
   if (n == 1) then
     -- Channel 1 Time
     apply_time_cursor(1, d)
     apply_pattern_division(1)
+    if params:get("reset_on_time_change") == 2 then
+      reset_loop(1)
+    end
   end
 
   if (n == 4) then
     apply_time_cursor(2, d)
     apply_pattern_division(2)
+    if params:get("reset_on_time_change") == 2 then
+      reset_loop(1)
+    end
   end
 
   refresh_arc()
@@ -268,12 +315,12 @@ function refresh_arc()
   local level = 10
   -- 1 feedback 
   f = params:get("1feedback")
-  fp = f/110
+  fp = f/100
   level = 15*fp
   a:segment(2, 0, tau*fp-0.001, level)
   -- 2 feedback
   f = params:get("2feedback")
-  fp = f/110
+  fp = f/100
   level = 15*fp
   a:segment(3, 0, tau*fp-0.001, level)
 
